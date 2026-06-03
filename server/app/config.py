6b0c9032a6e41e75a059271564ac6cb9aff0ac7b
DuongITMO <daomanhduong02112005@gmail.com>
2026-06-03 14:38:36 +0300

    Update config.py

--- server/app/config.py
+++ server/app/config.py
@@ -25,6 +25,7 @@ CORS_ALLOW_ORIGINS: list[str] = [
     "http://localhost:8080",  # Html
     "http://localhost:5173",
     "https://ml-service-six.vercel.app",
+    "https://ml-predict-app.vercel.app"
 ]
 CORS_ALLOW_METHODS: list[str] = ["*"]
 CORS_ALLOW_HEADERS: list[str] = ["*"]
